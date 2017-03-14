147dbdea667ba65e6a21cd2b906c889bad36de6b
SmithJGCardiff <SmithJG@Cardiff.ac.uk>
2017-03-14 20:40:19 +0000

    deleted old files

--- test.py
+++
@@ -1,7 +0,0 @@
-import shelve
-with shelve.open('questiondb') as db:
-	print('number of questions: '+str(len(db)))
-	for x in range(len(db)):
-		print('question no: '+str(x))
-		entry1 = db[str(x+1)]
-		print(entry1.entQuestion)
